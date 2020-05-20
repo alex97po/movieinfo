4d737a5bb9b7878e2f8e10f0778386524c667ac1
opohorelov <oleksandr.pohorelov@jevera.software>
2020-05-20 15:16:14 +0300

    Initial commit

---
+++ src/main/java/com/pogorelov/movieinfo/domain/Movie.java
@@ -0,0 +1,13 @@
+package com.pogorelov.movieinfo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Movie {
+
+    private String movieId;
+
+    private String name;
+}
---
+++ src/main/java/com/pogorelov/movieinfo/web/MovieResource.java
@@ -0,0 +1,18 @@
+package com.pogorelov.movieinfo.web;
+
+import com.pogorelov.movieinfo.domain.Movie;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+    @GetMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable String movieId) {
+        return new Movie(movieId, "Test name");
+    }
+
+}
